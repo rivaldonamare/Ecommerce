181f3fc37463bdb367e83a2830efd75e2838df3e
rivaldo <rivaldonamare8@gmail.com>
2024-01-31 00:52:07 +0700

    Complete Assignment 1

---
+++ EcommerceWEB.Models/Models/Product.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceWEB.Models.Models;
+
+public class Product
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required, MaxLength(50)]
+    public string Title { get; set; }
+    public string Description { get; set; }
+
+    [Required]
+    public string ISBN { get; set; }
+    [Required]
+    public string Author { get; set; }
+
+    [Required]
+    [Display(Name = "List Price")]
+    [Range(1, 1000000)]
+    public double ListPrice { get; set; }
+
+    [Required]
+    [Display(Name = "Price for 1-50")]
+    [Range(1, 1000000)]
+    public double Price { get; set; }
+
+    [Required]
+    [Display(Name = "Price for 50+")]
+    [Range(1, 1000000)]
+    public double Price50 { get; set; }
+
+    [Required]
+    [Display(Name = "Price for 100+")]
+    [Range(1, 1000000)]
+    public double Price100 { get; set; }
+}
